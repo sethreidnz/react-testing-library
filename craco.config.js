628fa16d0cbb9e8c2a099db7d3c800799d0a03aa
Seth Reid <seth.reid@xero.com>
2020-12-09 09:06:13 +1300

    updated craco

--- craco.config.js
+++ craco.config.js
@@ -1,14 +1,14 @@
-const CracoAlias = require('craco-alias');
+const CracoAlias = require("craco-alias");
 
 module.exports = {
   style: {
     postcss: {
       plugins: [
-        require('postcss-import'),
-        require('tailwindcss'),
-        require('postcss-nested'),
-        require('postcss-custom-properties'),
-        require('autoprefixer'),
+        require("postcss-import"),
+        require("tailwindcss"),
+        require("postcss-nested"),
+        require("postcss-custom-properties"),
+        require("autoprefixer"),
       ],
     },
   },
@@ -16,9 +16,9 @@ module.exports = {
     {
       plugin: CracoAlias,
       options: {
-        source: 'tsconfig',
-        baseUrl: './src',
-        tsConfigPath: './tsconfig.extend.json',
+        source: "tsconfig",
+        baseUrl: "./src",
+        tsConfigPath: "./tsconfig.extend.json",
       },
     },
   ],
